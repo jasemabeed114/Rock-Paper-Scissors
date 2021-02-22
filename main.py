2d6a3605000e921dd18d817a56a87a432c372c13
Omkaar <omkaar.nerurkar@gmail.com>
2021-02-22 15:29:22 +0000

    formatted

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import time
 import random
 print("ROCK, PAPER, SCISSORS")
 time.sleep(2)
-print("Rock wins against scissors. Scissors win against paper. Paper wins against rock. Enter 'R' for rock, 'P' for paper and 's' for scissors. You get 1 point per win and earn no points if you lose.")
+print(
+    "Rock wins against scissors. Scissors win against paper. Paper wins against rock. Enter 'R' for rock, 'P' for paper and 's' for scissors. You get 1 point per win and earn no points if you lose."
+)
 options = ["r", "p", "s"]
 noofrounds = int(input("Enter the number of rounds you want to play: "))
 ppoints = 0
@@ -14,9 +16,11 @@ while noofrounds != 0:
 	while pinput not in options:
 		pinput = input("Only enter 'R', 'P', or 'S': ")
 		pinput = pinput.lower()
-	if (pinput, mychoice) == ("p", "r") or (pinput, mychoice) == ("s", "p") or (pinput, mychoice) == ("r", "s"):
+	if (pinput, mychoice) == ("p", "r") or (pinput, mychoice) == (
+	    "s", "p") or (pinput, mychoice) == ("r", "s"):
 		ppoints += 1
-	if (pinput, mychoice) == ("r", "p") or (pinput, mychoice) == ("p", "s") or (pinput, mychoice) == ("s", "r"):
+	if (pinput, mychoice) == ("r", "p") or (pinput, mychoice) == (
+	    "p", "s") or (pinput, mychoice) == ("s", "r"):
 		mypoints += 1
 	if mychoice == "p":
 		print("The Computer chose 'Paper'!")
