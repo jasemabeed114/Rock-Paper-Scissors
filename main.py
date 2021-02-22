63aa38f272aa216c00e4c2cc617bba34b9635f7a
Omkaar <omkaar.nerurkar@gmail.com>
2021-02-22 07:17:44 +0000

    instructions added

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
+import time
 print("ROCK, PAPER, SCISSORS")
+time.sleep(2)
+print("Rock wins against scissors. Scissors win against paper. Paper wins against rock. Enter 'R' for rock, 'P' for paper and 's' for scissors.")
